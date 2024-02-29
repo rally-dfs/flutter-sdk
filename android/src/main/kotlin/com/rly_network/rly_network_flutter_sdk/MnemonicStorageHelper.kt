1763b0ac9be13ff0ff831cd79931985465442e1f
botdad <91175836+botdad@users.noreply.github.com>
2024-02-29 13:30:46 -0800

    move magic string to const

--- android/src/main/kotlin/com/rly_network/rly_network_flutter_sdk/MnemonicStorageHelper.kt
+++ android/src/main/kotlin/com/rly_network/rly_network_flutter_sdk/MnemonicStorageHelper.kt
@@ -8,6 +8,8 @@ import androidx.security.crypto.MasterKey
 import androidx.security.crypto.MasterKey.Builder
 import com.google.android.gms.auth.blockstore.*
 
+private const val ENCRYPTED_PREFERENCE_FILENAME = "encrypted_mnemonic"
+
 class MnemonicStorageHelper(context: Context) {
     private val sharedPreferences: SharedPreferences
     private val blockstoreClient: BlockstoreClient
@@ -22,7 +24,7 @@ class MnemonicStorageHelper(context: Context) {
         try {
             attemptedSharedPreferences = EncryptedSharedPreferences.create(
                 context,
-                "encrypted_mnemonic",
+                ENCRYPTED_PREFERENCE_FILENAME,
                 masterKey,
                 EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
@@ -32,11 +34,11 @@ class MnemonicStorageHelper(context: Context) {
             // the master key being invalidated. In this case, we need to clear the shared preferences
             // and recreate them. The user will lose their mnemonic, but this is the only way to
             // recover from this situation.
-            context.getSharedPreferences("encrypted_mnemonic", Context.MODE_PRIVATE).edit().clear().apply()
+            context.getSharedPreferences(ENCRYPTED_PREFERENCE_FILENAME, Context.MODE_PRIVATE).edit().clear().apply()
            
             attemptedSharedPreferences = EncryptedSharedPreferences.create(
                 context,
-                "encrypted_mnemonic",
+                ENCRYPTED_PREFERENCE_FILENAME,
                 masterKey,
                 EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                 EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
