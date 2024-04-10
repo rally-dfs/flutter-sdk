096a089d68f16797cb302ae8bd249263dbc88df3
Ryan Weald <ryan@weald.com>
2024-04-10 16:14:36 -0700

    Update mumbai network config to actually point at Amoy
    This is the "backwards compatible" version of the migration where devs
    don't need to fundamentally change their code.
    
    It should not be considered fully backwards compatible from an
    integration testing / usage standpoint though, as prior contracts, txns,
    addresses, etc will not exist on Amoy

--- lib/src/network_config/network_config_mumbai.dart
+++ lib/src/network_config/network_config_mumbai.dart
@@ -2,17 +2,17 @@ import './network_config.dart';
 
 final NetworkConfig mumbaiNetworkConfig = NetworkConfig(
   contracts: Contracts(
-    rlyERC20: '0x1C7312Cb60b40cF586e796FEdD60Cf243286c9E9',
-    tokenFaucet: '0xe7C3BD692C77Ec0C0bde523455B9D142c49720fF',
+    rlyERC20: '0x846d8a5fb8a003b431b67115f809a9b9fffe5012',
+    tokenFaucet: '0xb8c8274f775474f4f2549edcc4db45cbad936fac',
   ),
   gsn: GSNConfig(
-    paymasterAddress: '0x8b3a505413Ca3B0A17F077e507aF8E3b3ad4Ce4d',
-    forwarderAddress: '0xB2b5841DBeF766d4b521221732F9B618fCf34A87',
-    relayHubAddress: '0x3232f21A6E08312654270c78A773f00dd61d60f5',
+    paymasterAddress: '0xb570b57b821670707fF4E38Ea53fcb67192278F8',
+    forwarderAddress: '0x0ae8FC9867CB4a124d7114B8bd15C4c78C4D40E5',
+    relayHubAddress: '0xe213A20A9E6CBAfd8456f9669D8a0b9e41Cb2751',
     relayWorkerAddress: '0xb9950b71ec94cbb274aeb1be98e697678077a17f',
     relayUrl: 'https://api.rallyprotocol.com',
     rpcUrl:
-        'https://polygon-mumbai.g.alchemy.com/v2/-dYNjZXvre3GC9kYtwDzzX4N8tcgomU4',
+        'https://polygon-amoy.g.alchemy.com/v2/-dYNjZXvre3GC9kYtwDzzX4N8tcgomU4',
     chainId: '80001',
     maxAcceptanceBudget: '285252',
     domainSeparatorName: 'GSN Relayed Transaction',
