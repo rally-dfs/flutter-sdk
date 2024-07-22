37d7577aa0d90df498f55677a626f9402dcd4064
Cameron Morris <ctmorris@gmail.com>
2024-07-22 16:20:18 -0700

    updat relayworkeraddress

--- lib/src/network_config/network_config_base_sepolia.dart
+++ lib/src/network_config/network_config_base_sepolia.dart
@@ -9,7 +9,7 @@ final NetworkConfig baseSepoliaNetworkConfig = NetworkConfig(
     paymasterAddress: '0x0e20E8A953a1e7D5FD4B24F12aC021b6345F364F',
     forwarderAddress: '0xabf9Fa3b2b2d9bDd77f4271A0d5A309AA465BCBa',
     relayHubAddress: '0xb570b57b821670707fF4E38Ea53fcb67192278F8',
-    relayWorkerAddress: '0xbf385a38b766ea8f71f85a39837078c2c533fc5a',
+    relayWorkerAddress: '0xdb1d6c7b07c857cc22a4ef10ac7b1dd06dd7501f',
     relayUrl: 'https://api.rallyprotocol.com',
     rpcUrl:
         'https://base-sepolia.g.alchemy.com/v2/oOsX9gjRzWeq5WQrlM3zvWAXZ9nIT2Cr',
