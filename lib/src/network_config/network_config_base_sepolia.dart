5a8d816c9b9a23dd2c3ee154bc3158bff3ca8c98
Cameron <ctmorris@gmail.com>
2024-07-24 09:08:25 -0700

    update faucet contract address (#52)
    * update faucet contract
    
    * updat relayworkeraddress

--- lib/src/network_config/network_config_base_sepolia.dart
+++ lib/src/network_config/network_config_base_sepolia.dart
@@ -3,13 +3,13 @@ import './network_config.dart';
 final NetworkConfig baseSepoliaNetworkConfig = NetworkConfig(
   contracts: Contracts(
     rlyERC20: '0x16723e9bb894EfC09449994eC5bCF5b41EE0D9b2',
-    tokenFaucet: '0x1544267c4E158dFD81541fBbCcEBfCda96768A6E',
+    tokenFaucet: '0xCeCFB48a9e7C0765Ed1319ee1Bc0F719a30641Ce',
   ),
   gsn: GSNConfig(
     paymasterAddress: '0x0e20E8A953a1e7D5FD4B24F12aC021b6345F364F',
     forwarderAddress: '0xabf9Fa3b2b2d9bDd77f4271A0d5A309AA465BCBa',
     relayHubAddress: '0xb570b57b821670707fF4E38Ea53fcb67192278F8',
-    relayWorkerAddress: '0xbf385a38b766ea8f71f85a39837078c2c533fc5a',
+    relayWorkerAddress: '0xdb1d6c7b07c857cc22a4ef10ac7b1dd06dd7501f',
     relayUrl: 'https://api.rallyprotocol.com',
     rpcUrl:
         'https://base-sepolia.g.alchemy.com/v2/oOsX9gjRzWeq5WQrlM3zvWAXZ9nIT2Cr',
