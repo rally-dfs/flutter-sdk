29cc1c0d95aed637dc4cffced7b0ae6655ba7e6b
Ryan Weald <ryan@weald.com>
2025-01-23 08:02:00 -0800

    Pull example of using lens txn into code docs

--- lib/src/zksync/supported_chains.dart
+++ lib/src/zksync/supported_chains.dart
@@ -2,6 +2,26 @@ import 'package:eth_sig_util/model/typed_data.dart';
 import 'package:rly_network_flutter_sdk/src/zksync/zk_sync_chain.dart';
 
 /// Lens Protocol Network Sepolia Testnet
+/// Example usage:
+/// ```dart
+/// final transaction = ZKSyncEip712Transaction(
+///   from: wallet.address.hex,
+///   to: '0x111C3E89Ce80e62EE88318C2804920D4c96f92bb',
+///   nonce: BigInt.from(0),
+///   maxPriorityFeePerGas: BigInt.from(2),
+///   maxFeePerGas: BigInt.from(250000000000000),
+///   gas: BigInt.from(158774),
+///   value: BigInt.from(0),
+///   data: '0x',
+///   chainId: BigInt.from(37111),
+///   gasPerPubdata: BigInt.from(50000),
+/// );
+
+/// final lensTestNet = ZKSyncLensNetworkSepolia();
+
+/// await lensTestNet.sendTransaction(transaction, wallet);
+/// ```
+///
 class ZKSyncLensNetworkSepolia extends ZKSyncChain {
   ZKSyncLensNetworkSepolia()
       : super(
